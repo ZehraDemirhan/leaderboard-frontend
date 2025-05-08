9328daf254da1e4f13d270554987dca1d3abb7ea
Zehra Demirhan <zehra.demirhan@turkai.com>
2025-05-08 16:54:04 +0300

    pusher channels

--- src/lib/broadcast.ts
+++ src/lib/broadcast.ts
@@ -2,6 +2,7 @@ import Pusher from 'pusher-js';
 
 Pusher.logToConsole = true;
 
+//test
 export const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
         cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
         forceTLS: true,
