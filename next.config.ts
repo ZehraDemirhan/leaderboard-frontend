94d90ee561ec23e0a1b38ad8451dc8ba879169bf
Zehra Demirhan <zehra.demirhan@turkai.com>
2025-05-08 13:09:05 +0300

    add env-aware Axios baseURL (proxy HTTP through Next.js in prod)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/leaderboard/:path*',
+                destination: `${process.env.NEXT_PUBLIC_API_URL}/leaderboard/:path*`
+            }
+        ]
+    }
 };
 
 export default nextConfig;
