72adab9adbe2c6e189bc094e0e8bb21472f9ac6a
Zehra Demirhan <zehra.demirhan@turkai.com>
2025-05-08 15:34:42 +0300

    output: export specified

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    async rewrites() {
-        return [
-            {
-                source: '/api/v1/:path*',                               // catch all under /api/v1
-                destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*`
-            }
-        ]
-    },
+    output: 'export',
 };
 
 export default nextConfig;
